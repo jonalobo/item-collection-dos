28215ba07eea10232526c18d150724a908337672
Jonathan Alvarado <jonalobo98@gmail.com>
2022-08-14 22:49:44 -0600

    Listos los desafios

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,7 +9,7 @@ const Form = ({ handleChange, userData, placeOrder }) => {
           name="email"
           id="email"
           className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-          placeholder="Enter your email"
+          placeholder="Correo"
           defaultValue={userData.name}
         ></input>
       </div>
@@ -20,7 +20,7 @@ const Form = ({ handleChange, userData, placeOrder }) => {
           name="repEmail"
           id="repEmail"
           className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-          placeholder="Repeat your email"
+          placeholder="Repita el correo"
           defaultValue={userData.repEmail}
         ></input>
       </div>
@@ -32,7 +32,7 @@ const Form = ({ handleChange, userData, placeOrder }) => {
             name="name"
             id="name"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder="Enter your name"
+            placeholder="Su nombre"
             defaultValue={userData.name}
           ></input>
         </div>
@@ -42,7 +42,7 @@ const Form = ({ handleChange, userData, placeOrder }) => {
             name="phone"
             id="phone"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder="Phone Number "
+            placeholder="Número de teléfono "
             defaultValue={userData.phone}
           ></input>
         </div>
